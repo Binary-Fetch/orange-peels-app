3dac0970cfbe1d148fa8b0132f279f137b59759b
Kushal Biswas <biswas.kushal@gmail.com>
2021-01-18 01:10:35 +0530

    url change

--- cocktail-app/constants/Config.ts
+++ cocktail-app/constants/Config.ts
@@ -1,5 +1,5 @@
 export default class AppConfiguration {
-    static readonly GQLBackendUrl = 'https://cocktail-api-astra.herokuapp.com/api/graphql';
+    static readonly GQLBackendUrl = 'https://orange-peels-api.herokuapp.com/api/graphql';
     static readonly storageKeyForAuth = 'AUTH_KEY';
     static readonly storageKeyForUserDetails = 'USER_DETAILS_KEY';
     static readonly loginFailedMessage = 'Login failed due to invalid credential';
